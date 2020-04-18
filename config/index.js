7dbfbc21803dd3e2ae722722ae547a85c74e22b1
Maigret Aurélien <dewep@pandalab.fr>
2020-04-18 20:29:09 +0200

    Add config module

---
+++ config/index.js
@@ -0,0 +1,56 @@
+const configs = require('./config')
+
+const defaultConfiguration = {
+  gce: {
+    hosts: [],
+    port: 6730,
+    secure: true
+  }
+}
+
+/******************************************************************
+*********** Source: https://stackoverflow.com/a/34749873 **********
+******************************************************************/
+
+/**
+ * Simple object check.
+ * @param item
+ * @returns {boolean}
+ */
+function isObject (item) {
+  return (item && typeof item === 'object' && !Array.isArray(item))
+}
+
+/**
+ * Deep merge two objects.
+ * @param target
+ * @param ...sources
+ */
+function mergeDeep (target, ...sources) {
+  if (!sources.length) {
+    return target
+  }
+
+  const source = sources.shift()
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) {
+          Object.assign(target, { [key]: {} })
+        }
+        mergeDeep(target[key], source[key])
+      } else {
+        Object.assign(target, { [key]: source[key] })
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources)
+}
+
+/******************************************************************
+*******************************************************************
+******************************************************************/
+
+module.exports = mergeDeep(defaultConfiguration, ...configs)
