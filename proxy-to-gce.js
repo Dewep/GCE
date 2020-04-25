5cd1ae637d9bedc52e97ad9e2747640a8ab401c9
Maigret Aurélien <dewep@pandalab.fr>
2020-04-25 16:31:34 +0200

    Fix proxy to gce pipe

--- proxy-to-gce.js
+++ proxy-to-gce.js
@@ -1,11 +1,15 @@
 const net = require('net')
 const config = require('./config')
 
-net.createServer(function (client) {
+const server = net.createServer(function (client) {
   client.once('data', function (buf) {
     const proxy = net.createConnection(config.gce.ports.loadBalancer, function () {
       proxy.write(buf)
       client.pipe(proxy).pipe(client)
     })
+    proxy.on('error', console.warn)
   })
-}).listen(+(process.argv[2] || '443'))
+  client.on('error', console.warn)
+})
+server.listen(+(process.argv[2] || '443'))
+server.on('error', console.warn)
