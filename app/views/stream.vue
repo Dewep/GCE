a019ac4ea1ef18524590f93c44300a3eb13ebccd
Maigret Aurélien <dewep@pandalab.fr>
2020-04-27 18:07:36 +0200

    Use v-deep

--- app/views/stream.vue
+++ app/views/stream.vue
@@ -85,36 +85,36 @@ export default {
   word-break: break-all;
 }
 
-.output >>> .ansi-black-fg { color: #3A3A3A; }
-.output >>> .ansi-red-fg { color: #DD948E; }
-.output >>> .ansi-green-fg { color: #B6D1AA; }
-.output >>> .ansi-yellow-fg { color: #F3D57C; }
-.output >>> .ansi-blue-fg { color: #8AA9D5; }
-.output >>> .ansi-magenta-fg { color: #CBAFD5; }
-.output >>> .ansi-cyan-fg { color: #9AD1D4; }
-.output >>> .ansi-white-fg { color: #EFEFEF; }
-.output >>> .ansi-bright-black-fg { color: #5E5D5E; }
-.output >>> .ansi-bright-red-fg { color: #E69B94; }
-.output >>> .ansi-bright-green-fg { color: #D1F0C3; }
-.output >>> .ansi-bright-yellow-fg { color: #F4D799; }
-.output >>> .ansi-bright-blue-fg { color: #A6CBFE; }
-.output >>> .ansi-bright-magenta-fg { color: #E7C7F2; }
-.output >>> .ansi-bright-cyan-fg { color: #B0F0F5; }
-.output >>> .ansi-bright-white-fg { color: #FFF; }
-.output >>> .ansi-black-bg { background-color: #3A3A3A; }
-.output >>> .ansi-red-bg { background-color: #DD948E; }
-.output >>> .ansi-green-bg { background-color: #B6D1AA; }
-.output >>> .ansi-yellow-bg { background-color: #F3D57C; }
-.output >>> .ansi-blue-bg { background-color: #8AA9D5; }
-.output >>> .ansi-magenta-bg { background-color: #CBAFD5; }
-.output >>> .ansi-cyan-bg { background-color: #9AD1D4; }
-.output >>> .ansi-white-bg { background-color: #EFEFEF; }
-.output >>> .ansi-bright-black-bg { background-color: #5E5D5E; }
-.output >>> .ansi-bright-red-bg { background-color: #E69B94; }
-.output >>> .ansi-bright-green-bg { background-color: #D1F0C3; }
-.output >>> .ansi-bright-yellow-bg { background-color: #F4D799; }
-.output >>> .ansi-bright-blue-bg { background-color: #A6CBFE; }
-.output >>> .ansi-bright-magenta-bg { background-color: #E7C7F2; }
-.output >>> .ansi-bright-cyan-bg { background-color: #B0F0F5; }
-.output >>> .ansi-bright-white-bg { background-color: #FFF; }
+.output::v-deep(.ansi-black-fg) { color: #3A3A3A; }
+.output::v-deep(.ansi-red-fg) { color: #DD948E; }
+.output::v-deep(.ansi-green-fg) { color: #B6D1AA; }
+.output::v-deep(.ansi-yellow-fg) { color: #F3D57C; }
+.output::v-deep(.ansi-blue-fg) { color: #8AA9D5; }
+.output::v-deep(.ansi-magenta-fg) { color: #CBAFD5; }
+.output::v-deep(.ansi-cyan-fg) { color: #9AD1D4; }
+.output::v-deep(.ansi-white-fg) { color: #EFEFEF; }
+.output::v-deep(.ansi-bright-black-fg) { color: #5E5D5E; }
+.output::v-deep(.ansi-bright-red-fg) { color: #E69B94; }
+.output::v-deep(.ansi-bright-green-fg) { color: #D1F0C3; }
+.output::v-deep(.ansi-bright-yellow-fg) { color: #F4D799; }
+.output::v-deep(.ansi-bright-blue-fg) { color: #A6CBFE; }
+.output::v-deep(.ansi-bright-magenta-fg) { color: #E7C7F2; }
+.output::v-deep(.ansi-bright-cyan-fg) { color: #B0F0F5; }
+.output::v-deep(.ansi-bright-white-fg) { color: #FFF; }
+.output::v-deep(.ansi-black-bg) { background-color: #3A3A3A; }
+.output::v-deep(.ansi-red-bg) { background-color: #DD948E; }
+.output::v-deep(.ansi-green-bg) { background-color: #B6D1AA; }
+.output::v-deep(.ansi-yellow-bg) { background-color: #F3D57C; }
+.output::v-deep(.ansi-blue-bg) { background-color: #8AA9D5; }
+.output::v-deep(.ansi-magenta-bg) { background-color: #CBAFD5; }
+.output::v-deep(.ansi-cyan-bg) { background-color: #9AD1D4; }
+.output::v-deep(.ansi-white-bg) { background-color: #EFEFEF; }
+.output::v-deep(.ansi-bright-black-bg) { background-color: #5E5D5E; }
+.output::v-deep(.ansi-bright-red-bg) { background-color: #E69B94; }
+.output::v-deep(.ansi-bright-green-bg) { background-color: #D1F0C3; }
+.output::v-deep(.ansi-bright-yellow-bg) { background-color: #F4D799; }
+.output::v-deep(.ansi-bright-blue-bg) { background-color: #A6CBFE; }
+.output::v-deep(.ansi-bright-magenta-bg) { background-color: #E7C7F2; }
+.output::v-deep(.ansi-bright-cyan-bg) { background-color: #B0F0F5; }
+.output::v-deep(.ansi-bright-white-bg) { background-color: #FFF; }
 </style>
